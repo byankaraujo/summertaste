10f6ad479ee325ca293e488294602bfce846cff7
byankaraujo <araujomyrelly@gmail.com>
2026-03-01 19:14:19 -0300

    Configuração inicial do projeto Spring Boot

---
+++ src/main/java/br/com/summertaste/SummerTasteApplication.java
@@ -0,0 +1,13 @@
+package br.com.summertaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerTasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerTasteApplication.class, args);
+	}
+
+}
